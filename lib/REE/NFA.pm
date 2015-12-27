c74eb577bf527315306d8e26bdc56ea009ba1e0c
Mirko Westermeier <mirko@westermeier.de>
2015-12-27 11:30:40 +0100

    implement NFA final state handling

--- lib/REE/NFA.pm
+++ lib/REE/NFA.pm
@@ -5,6 +5,7 @@ has name            => 'unnamed NFA';
 has states          => (builder => '_init_states');
 has _state_counter  => 1;
 has state           => sub {shift->get_start};
+has _final          => {};
 
 sub _init_states {
     shift->states(['q_0']);
@@ -18,6 +19,26 @@ sub get_start {
 sub is_start {
     my ($self, $state) = @_;
     return $state eq $self->get_start;
+}
+
+sub set_final {
+    my ($self, $state) = @_;
+    $self->_final->{$state} = 1;
+}
+
+sub unset_final {
+    my ($self, $state) = @_;
+    $self->_final->{$state} = '';
+}
+
+sub is_final {
+    my ($self, $state) = @_;
+    return $self->_final->{$state};
+}
+
+sub is_done {
+    my $self = shift;
+    return $self->is_final($self->state);
 }
 
 1;
