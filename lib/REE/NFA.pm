a2fe937fe4e0903b944a1fdbbfe85b84e7fd9bfa
Mirko Westermeier <mirko@westermeier.de>
2015-12-27 11:14:30 +0100

    implement very basic NFA features for the first trivial tests

--- lib/REE/NFA.pm
+++ lib/REE/NFA.pm
@@ -1,4 +1,24 @@
 package REE::NFA;
-use REE::Mo 'default';
+use REE::Mo qw(default builder);
 
-# TODO Konstruktor
+has name            => 'unnamed NFA';
+has states          => (builder => '_init_states');
+has _state_counter  => 1;
+has state           => sub {shift->get_start};
+
+sub _init_states {
+    shift->states(['q_0']);
+}
+
+sub get_start {
+    my $self = shift;
+    return $self->states->[0]; # first = start
+}
+
+sub is_start_state {
+    my ($self, $state) = @_;
+    return $state eq $self->get_start;
+}
+
+1;
+__END__
