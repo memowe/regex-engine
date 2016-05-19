e0b324d6d386e4bf6b7b1c17cafadb966c191334
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2016-05-19 15:59:21 +0200

    add repetition regex class

---
+++ lib/REE/RE/Repetition.pm
@@ -0,0 +1,19 @@
+package REE::RE::Repetition;
+use REE::Mo 'required';
+extends 'REE::RE';
+
+has re => (required => 1);
+
+sub to_string {
+    my ($self, $indent) = @_;
+    $indent //= '';
+
+    my $output = $indent . "REPETITION: \n";
+    $output .= $self->re->to_string("$indent    ");
+    return $output;
+}
+
+sub to_regex {
+    my $self = shift;
+    return $self->re->to_regex . '*';
+}
