9b92492ff16ecb83e637162b7e76b04bdc2f81b0
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2016-05-19 16:41:06 +0200

    add first parser draft with tests

--- lib/REE/Parser.pm
+++ lib/REE/Parser.pm
@@ -1,5 +1,9 @@
 package REE::Parser;
 use REE::Mo;
+use REE::RE::Alternation;
+use REE::RE::Sequence;
+use REE::RE::Repetition;
+use REE::RE::Literal;
 
 has _input => undef;
 
@@ -9,6 +13,7 @@ sub _next_char {
     # extract first character
     my $input = $self->_input;
     my $first = substr $input, 0, 1;
+    return unless length $first == 1;
 
     # store remaining string
     $self->_input(substr $input, 1);
@@ -26,7 +31,51 @@ sub parse {
 sub parse_alternation {
     my $self = shift;
 
-    my @sub_re_sequences = (REE::RE::Sequence->new(res => []));
+    # parse sequences
+    my @sequences = (REE::RE::Sequence->new(res => []));
     while (defined(my $c = $self->_next_char)) {
+        my $buffer;
+
+        # new alternative
+        if ($c eq '|') {
+            push @sequences, REE::RE::Sequence->new(res => []);
+        }
+
+        # sub-alternation
+        elsif ($c eq '(') {
+            $buffer = $self->parse_alternation;
+        }
+
+        # alternation finished
+        elsif ($c eq ')') {
+            last;
+        }
+
+        # repetition of previous re
+        elsif ($c eq '*') {
+
+            # get previous re
+            my $cur_seq = $sequences[-1];
+            die "unexpected *\n" unless $cur_seq;
+            my $cur_re  = pop @{$cur_seq->res};
+            die "unexpected *\n" unless $cur_re;
+
+            # inject repeated re
+            push @{$cur_seq->res}, REE::RE::Repetition->new(re => $cur_re);
+        }
+
+        # literal
+        else {
+            $buffer = REE::RE::Literal->new(value => $c);
+        }
+
+        # add buffer to last sequence
+        if (defined $buffer) {
+            my $current = $sequences[-1];
+            push @{$current->res}, $buffer;
+        }
     }
+
+    # collect sequences
+    return REE::RE::Alternation->new(res => \@sequences)->simplified;
 }
